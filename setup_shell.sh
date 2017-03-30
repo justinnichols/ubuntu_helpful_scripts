c7737dd02d88de8e0bb6d426c7d88589b322ec45
Justin Nichols <justin@nichols.mobi>
2017-03-29 20:16:43 -0400

    Added TERM to .zshrc

--- setup_shell.sh
+++ setup_shell.sh
@@ -18,6 +18,9 @@ echo "Setting Oh-My-Zsh Theme.";
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k ;
 sed -i 's/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel9k\/powerlevel9k\"/g' ~/.zshrc ;
 
+echo "Prepending TERM setting to ~/.zshrc";
+echo "export TERM=\"xterm-256color\"" | cat - ~/.zshrc > temp && mv temp ~/.zshrc
+
 echo "Appending other ~/.zshrc options.";
 echo "" >> ~/.zshrc;
 echo "export EDITOR=vi" >> ~/.zshrc;
